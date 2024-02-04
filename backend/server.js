7eff444ff1c23082aeb4b6d00a85071ce79b2fcb
kennotfindsymbol <kennotfindsymbol@gmail.com>
2024-02-04 22:01:37 +0800

    Create mongo connection

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
 // express app
@@ -15,6 +16,14 @@ app.use((req, res, next) => {
 
 app.use('/api/workouts', workoutRoutes)
 
-app.listen(process.env.PORT, () => {
-  console.log(`listening on port ${process.env.PORT}`)
-})
+mongoose.connect(process.env.MONG_URI)
+  .then(() => {
+    console.log('connected to Mongo');
+    app.listen(process.env.PORT, () => {
+      console.log(`listening on port ${process.env.PORT}`)
+    })
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+
