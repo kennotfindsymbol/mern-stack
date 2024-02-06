014d5b36bad73120d60b73cbcb0ff579c099989e
kennotfindsymbol <kennotfindsymbol@gmail.com>
2024-02-07 02:02:43 +0800

    Add signup and login pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 // pages and components
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
@@ -14,7 +16,15 @@ function App() {
             <Route
               path="/"
               element={<Home />}
-              />
+            />
+            <Route
+              path="/login"
+              element={<Login />}
+            />
+            <Route
+              path="/signup"
+              element={<Signup />}
+            />
           </Routes>
         </div>
       </BrowserRouter>
