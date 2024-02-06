21ce9befe8eef97df07b9243cbc053590de2eb9b
kennotfindsymbol <kennotfindsymbol@gmail.com>
2024-02-06 23:37:49 +0800

    Create jwt create

---
+++ backend/controllers/userController.js
@@ -0,0 +1,29 @@
+const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+const craeteToken = (_id) => {
+  return jwt.sign({_id}, process.env.SECRET, { expiresIn: '1h' })
+}
+
+const loginUser = async (req, res) => {
+  res.json({mssg: 'Login user'})
+}
+
+const signupUser = async (req, res) => {
+  const {email, password} = req.body
+
+  try {
+    const user = await User.signup(email, password)
+    // create a token
+    const token = craeteToken(user._id)
+
+    res.status(200).json({email, token})
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
+}
+
+module.exports = { 
+  signupUser, 
+  loginUser,
+}
